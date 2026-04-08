efe1cee6abafba3cd2520e7ba18ea885bb1dfa4b
Benjamin Wheeler <benjaminwbusiness@gmail.com>
2026-04-07 20:25:32 -0500

    imported and implemented SQLalchemy basics. To be used later.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from app.schemas import Job
+from sqlalchemy import create_engine, text 
 
+# This is the SQLAlchemy create engine command that will use a sqlite data schema
+# and store the info in memory. Echo prompts the terminal/logger to confirm commands as ran
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
 app = FastAPI()
 
@@ -26,8 +30,9 @@ def get_jobs():
 # This grabs the job by job_id
 @app.get("/jobs/{job_id}")
 def get_job_by_id(job_id: int):
-    job = find_job(job_id)
-    return job
+    for index, job in enumerate(jobs):
+        if job["id"] == job_id:
+            return index, job
 
 # This deletes the job by job_id
 @app.delete("/jobs/{job_id}")
@@ -46,15 +51,35 @@ def update_job(job_id: int, job: Job):
 
     for index, existing_job in enumerate(jobs):
         if existing_job["id"] == job_id:
-            job[index] = updated_job
+            jobs[index] = updated_job
             return updated_job
     
     raise HTTPException(status_code=404, detail="Job not found")
 
 
-#call helper
-def find_job(job_id: int):
-    for index, job in enumerate(jobs):
-        if job["id"] == job_id:
-            return index, job
-    return None, None
+#SQL Engine Section 
+'''This creates an engine object and the engine object creates a database and a table
+Then we execute a command that INSERTs data into the table and commits it
+Committing allows us to build a database that can go on to accept more transactions
+This runs everytime we start the application and only saves the data in memory.
+'''
+with engine.connect() as conn:
+    result = conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+        )
+    conn.commit()
+
+# using engine.begin will auto commit if the query passes
+with engine.begin() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
+    )
+
+# This statement just selects data from the database and prints it
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table"))
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
